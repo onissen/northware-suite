5ca3a53167e110706e6d44df78b987a34a83e0ac
Oke <56229919+onissen@users.noreply.github.com>
2023-01-08 14:15:04 +0100

    Next Themes Works good with default config

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 /* jshint esversion:6 */
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { ThemeProvider } from 'next-themes';
 config.autoAddCss = false;
-
 import '../styles/globals.sass';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return(
+    <ThemeProvider forcedTheme={Component.theme || 'light'} enableSystem="false">
+      {/* TODO: Hier als Fallback das Cockpit Theme hinterlegen */}
+      <Component {...pageProps} />
+    </ThemeProvider>
+    )
 }
+
+export default App;
