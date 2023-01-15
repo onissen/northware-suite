3dc3cfc33c9fc77abc30b64c80cd9766feb14868
Oke <56229919+onissen@users.noreply.github.com>
2023-01-15 01:57:49 +0100

    FOrm Floating for LoginForm

--- components/specific/LoginForm.js
+++ components/specific/LoginForm.js
@@ -14,7 +14,17 @@ export default function LoginForm(props) {
         {!loading && !user ? (
           <div className="p-9" >
             <h1 className='text-center'>Login</h1>
-            <form onSubmit={props.handleSubmit} className="">
+            <form onSubmit={props.handleSubmit} className="flex flex-col">
+              <div class="form-floating my-3">
+                  <input type="text" name="identifier" id="input-identifier" onChange={props.handleChange} required class="form-control-lg peer" placeholder=" " />
+                  <label for="input-identifier" class="peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Benutzername</label>
+              </div>
+
+              <div class="form-floating my-3">
+                  <input type="text" name="password" id="input-password" onChange={props.handleChange} required class="form-control-lg peer" placeholder=" " />
+                  <label for="input-password" class="peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Passwort</label>
+              </div>
+
               <input className="form-control-lg my-3" type="text" name="identifier" onChange={props.handleChange} placeholder="Benutzername" required />
               <input className="form-control-lg my-3" type="password" name="password" onChange={props.handleChange} placeholder="Passwort" required />
 
