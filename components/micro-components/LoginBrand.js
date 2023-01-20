0550e353a3b096969f24a4f66829f74f72b61a12
Oke <56229919+onissen@users.noreply.github.com>
2023-01-20 16:56:16 +0100

    ...

--- components/micro-components/LoginBrand.js
+++ components/micro-components/LoginBrand.js
@@ -1,10 +1,10 @@
 /*jshint esversion:6 */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import styles from '../../styles/components/LoginForm.module.sass';
+import design from '../../styles/components/LoginForm.module.sass';
 
 export default function LoginBrand(props) {
     return (
-        <div className={`${props.bgClass} mb-4 ${styles.loginBrand}`}>
+        <div className={`${props.bgClass} mb-4 ${design.loginBrand}`}>
           <FontAwesomeIcon icon={props.brandIcon} className="mr-2"/>
           <span>{props.brandName}</span>
         </div>
