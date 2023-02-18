220c323efd7879739cc6c8aa6a32bdf4e0ca7811
Oke <56229919+onissen@users.noreply.github.com>
2023-02-18 19:26:12 +0100

    Thaughts good I think doesnt work anyway

--- components/common/HeadNav.js
+++ components/common/HeadNav.js
@@ -6,6 +6,7 @@ import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, ChevronDownIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { unsetToken } from '../../lib/auth';
+import NavBarContent from '../micro-components/HeadNav_navbarContent';
 const logout = () => {
   unsetToken();
 };
@@ -90,6 +91,7 @@ export default function HeadNav(props) {
                       ) : ('')}
                     </Menu>
                   ))}
+                  <NavBarContent service={props.service} />
                 </div>
               </div>
 
