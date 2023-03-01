143bb51221e02c45641c94a1769a92b60374d04c
Oke <56229919+onissen@users.noreply.github.com>
2023-03-01 19:10:56 +0100

    Tabs work again

--- components/common/NewTab.js
+++ components/common/NewTab.js
@@ -3,13 +3,10 @@ import React, { useState } from 'react';
 export function NewTab() {
   const [activeTab, setActiveTab] = useState(0);
   const [tabs, setTabs] = useState([]);
-  const [panels, setPanels] = useState([]);
 
   function handleAddTab(e) {
-    const newTab = e.target.name;
-    const newPanel = e.target.component;
+    const newTab = e.target.getAttribute('label');
     setTabs([...tabs, newTab]);
-    setPanels([...panels, newPanel]);
     setActiveTab(tabs.length); // Jump to the new tab
   }
 
@@ -20,19 +17,8 @@ export function NewTab() {
           <li key={index} className={activeTab === index ? 'active' : ''}>{tab}</li>
         ))}
       </ul>
-      {panels.map((panel, index) => {
-        if (panel == index)
-          return (
-            <div key={panel.props.label} className={design.TabPanel}>
-                {panel.props.children}
-            </div>
-          )
-      })}
-      <button class="btn btn-success" onClick={handleAddTab} label="test" component="Test">Add Tab</button>
+      
+      <button class="btn btn-success" onClick={handleAddTab} label="TesterLabel">Add Tab</button>
     </div>
   );
-}
-
-export function Test () {
-    return('Test');
 }
